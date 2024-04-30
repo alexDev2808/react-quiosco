9c59852addd9725b821d4b97a95daf9c689a2c7e
alexhDev2808 <colores.2808.8000@gmail.com>
2024-04-30 12:23:19 -0600

    Mostrando productos

--- src/views/Inicio.jsx
+++ src/views/Inicio.jsx
@@ -1,6 +1,22 @@
+import Producto from '../components/Producto'
+import { productos } from '../data/productos'
 
 export default function Inicio() {
   return (
-    <div>Inicio</div>
+    <>
+      <h1 className='text-4xl font-bold'>Inicio</h1>
+      <p className='text-2xl my-10'>
+        Elige y personaliza tu pedido a continuacion.
+      </p>
+
+      <div className='grid gap-4 grid-cols-1 lg:grid-cols-2 xl:grid-cols-3'>
+        {productos.map(producto => (
+          <Producto 
+            key={producto.imagen} 
+            producto={producto}
+          />
+        ))}
+      </div>
+    </>
   )
 }
