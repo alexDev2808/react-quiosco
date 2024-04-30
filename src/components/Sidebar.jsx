7c9bc5c8d4e4b2fc6e404b218237f66dc5326e69
alexhDev2808 <colores.2808.8000@gmail.com>
2024-04-30 11:24:45 -0600

    Creando secciones para el Layout principal

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,16 @@
+
+
+export default function Sidebar() {
+  return (
+    <aside className="md:w-72">
+        <div className="p-4">
+            <img 
+                src="img/logo.svg" 
+                alt="Logotipo" 
+            />
+        </div>
+
+        
+    </aside>
+  )
+}
